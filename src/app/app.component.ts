3b79ec5cfb0a954fd9aa67dd6ec01bbbf4fb031b
Henry Fritz <henryamsterfritz@gmail.com>
2021-10-15 08:18:26 -0400

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FrontEndDevTest';
+  title = 'Front End Dev Test for Training Resource Center';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,59 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { ReadMeComponent } from './read-me/read-me.component';
+import { MatButtonModule, } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatSortModule} from '@angular/material/sort';
+import { MatInputModule} from '@angular/material/input';
+import { MatDialogModule} from '@angular/material/dialog'; 
+import { MatTabsModule} from '@angular/material/tabs'; 
+import { MatExpansionModule} from '@angular/material/expansion'; 
+import { MatDividerModule} from '@angular/material/divider'; 
+import { MatStepperModule} from '@angular/material/stepper'; 
+import { MatRadioModule} from '@angular/material/radio'; 
+import { MatTreeModule} from '@angular/material/tree';  
+import { MatBottomSheetModule} from '@angular/material/bottom-sheet'; 
+import { MatProgressBarModule} from '@angular/material/progress-bar';
+import { MatSelectModule} from '@angular/material/select'; 
+import {MatTableModule } from '@angular/material/table';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.modules';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReadMeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatDialogModule,
+    MatSortModule,
+    MatDialogModule,
+    MatTabsModule,
+    MatExpansionModule,
+    MatDividerModule,
+    MatStepperModule,
+    MatTreeModule,
+    MatBottomSheetModule,
+    MatProgressBarModule,
+    FormsModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatInputModule,
+    MatTableModule,
+    MatBottomSheetModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
