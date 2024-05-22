3642921fb4d7b0bf54a04e2dcb44f9281ea541f0
Fritz <Henry.Fritz@eku.edu>
2024-05-22 12:52:02 -0400

    replaced and rescaffolded over v12

---
+++ copy/app/read-me/read-me.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-read-me',
+  templateUrl: './read-me.component.html',
+  styleUrls: ['./read-me.component.css']
+})
+export class ReadMeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  title="DevTest"
+}
